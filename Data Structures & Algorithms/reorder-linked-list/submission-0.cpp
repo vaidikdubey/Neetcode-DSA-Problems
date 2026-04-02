63a6bd87385a10c91a3a0af7045f5001095aaec7
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-02 12:07:03 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+    ListNode *reverseLL(ListNode *head) {
+        ListNode *prev = NULL, *curr = head;
+
+        while(curr) { //O(n)
+            ListNode *next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+public:
+    void reorderList(ListNode* head) {
+        //Find mid of linked list
+        ListNode *slow = head, *fast = head;
+
+        while(fast && fast -> next) { //O(n)
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+
+        /* Move to first value of second half and terminate the link to make
+        separate lists */
+        ListNode *second = slow -> next;
+        slow -> next = NULL;
+        
+        ListNode *head1 = head;
+        ListNode *head2 = reverseLL(second); //Reverse the second linked list
+        
+        //Merge both linked lists as per conditions
+        while(head1 && head2) { //O(n)
+            ListNode *next1 = head1 -> next;
+            head1 -> next = head2;
+            head1 = next1;
+
+            ListNode *next2 = head2 -> next;
+            head2 -> next = head1;
+            head2 = next2;
+        } //T.C: O(n), S.C: O(1)
+    }
+};
