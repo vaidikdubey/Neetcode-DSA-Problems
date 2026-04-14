c1f79f1ba3e283b375aed9b98803aba00313533c
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-04-14 12:11:12 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+
+        for(int i = 0; i < position.size(); i++) { //O(n)
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.rbegin(), cars.rend()); //O(nlogn)
+
+        int fleets = 0;
+        double lastTime = 0;
+        
+        for(auto &car: cars) { //O(n)
+            double time = (double)(target - car.first) / car.second;
+            
+            if(time > lastTime) {
+                fleets++;
+                lastTime = time;
+            }
+        }
+        return fleets; //T.C: O(nlogn), S.C: O(1)
+    }
+};
